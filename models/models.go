c1f52f9b3edb10de538d1b487bbd2e84c1443e91
timdong <tim.dong@hotmail.com>
2021-02-20 19:45:13 +0800

    no changes

--- models/models.go
+++ models/models.go
@@ -7,8 +7,9 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 
-	"github.com/EDDYCJY/go-gin-example/pkg/setting"
 	"time"
+
+	"github.com/EDDYCJY/go-gin-example/pkg/setting"
 )
 
 var db *gorm.DB
@@ -23,14 +24,16 @@ type Model struct {
 // Setup initializes the database instance
 func Setup() {
 	var err error
-	db, err = gorm.Open(setting.DatabaseSetting.Type, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	dbStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		setting.DatabaseSetting.User,
 		setting.DatabaseSetting.Password,
 		setting.DatabaseSetting.Host,
-		setting.DatabaseSetting.Name))
+		setting.DatabaseSetting.Name)
+	db, err = gorm.Open(setting.DatabaseSetting.Type, dbStr)
 
 	if err != nil {
 		log.Fatalf("models.Setup err: %v", err)
+		log.Fatalf("db link : %v \n", dbStr)
 	}
 
 	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
